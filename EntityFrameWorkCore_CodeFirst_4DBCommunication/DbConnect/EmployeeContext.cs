a0e3679fb22e97bb04d980ae09bff1c140a9789c
venkat <dell@DESKTOP-S8CP3CH>
2026-04-12 11:14:47 +0530

    Add project files.

---
+++ EntityFrameWorkCore_CodeFirst_4DBCommunication/DbConnect/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameWorkCore_CodeFirst_4DBCommunication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameWorkCore_CodeFirst_4DBCommunication.DbConnect
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options) 
+        {
+            
+        }
+        //you need to register model class into DbSet<>placeholder section.
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
